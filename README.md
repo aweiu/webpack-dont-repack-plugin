8ec0f2273ff7fdef48ca819abdf493ce4b6875fa
aweiu <328080339@qq.com>
2022-09-01 17:16:40 +0800

    chore: first-blood

---
+++ README.md
@@ -0,0 +1,60 @@
+# webpackDontRepackPlugin
+
+One line of code to solve repeated packaging of webpack
+
+![Example 1](examples/1.png)
+
+## Install
+
+```
+npm install webpack-dont-repack-plugin
+```
+
+## Use
+
+```
+// Your webpack config
+{
+  ...
+  plugins: [
+      new WebpackSplitChunksPlugin()
+  ],
+  ...
+}
+```
+
+## Config
+
+```
+new WebpackSplitChunksPlugin({
+  log: false // default: true
+})
+```
+
+If `log: true`, you will see in the console after the build is completed:
+
+![Example 2](examples/2.png)
+
+## Risk
+
+I wonder if you are curious about this: why does the webpack official not directly repair such an obvious and long-standing problem? One possible explanation is "historical baggage", that is, solving the problem of repeated packaging may produce additional side effects.
+
+What side effects can a module with the same name and version be packaged into one?
+
+This is also something I came up with by chance. I directly put the code on it. It is assumed that this is a module that was originally packaged into multiple copies and later packaged into one:
+
+```javascript
+let i = 1;
+
+function test() {
+  console.log(i++);
+}
+
+export default test;
+```
+
+After reading the code, you should have understood it.
+
+After the module package becomes one, the referenced instance also becomes one. In fact, it is not easy to say whether this is a side effect, because the expectation of each dependency may be one instance, but because of the Download Strategy of the existing package management tool, it has become multiple.
+
+**So, It still needs more testing.**
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { statSync } from "fs";
+import { Compiler } from "webpack";
+import { formatBytes } from "./helpers";
+
+export interface WebpackDontRepackPluginOptions {
+  log?: boolean;
+}
+
+class WebpackDontRepackPlugin {
+  private processed = new Set<string>();
+  private pathCache = new Map<string, string>();
+  private options: WebpackDontRepackPluginOptions;
+
+  constructor(options: WebpackDontRepackPluginOptions = {}) {
+    this.options = options;
+  }
+
+  apply(compiler: Compiler) {
+    this.applyDontRepack(compiler);
+    if (this.options.log !== false) {
+      this.applyLog(compiler);
+    }
+  }
+
+  private applyDontRepack(compiler: Compiler) {
+    compiler.resolverFactory.hooks.resolver
+      .for("normal")
+      .tap("name", (resolver) => {
+        resolver.hooks.result.tap(this.constructor.name, (result) => {
+          const { descriptionFileData, path, relativePath } = result;
+          // @ts-ignore
+          const { name, _id } = descriptionFileData;
+          const key = _id || name;
+
+          if (key && path) {
+            const id = key + relativePath; // key + 相对路径作为模块唯一索引
+            const cachePath = this.pathCache.get(id);
+
+            if (cachePath === undefined) {
+              this.pathCache.set(id, path);
+            } else if (path !== cachePath) {
+              this.processed.add(path);
+              result.path = cachePath;
+            }
+          }
+
+          return result;
+        });
+      });
+  }
+
+  private applyLog(compiler: Compiler) {
+    const logger = compiler.getInfrastructureLogger(this.constructor.name);
+
+    compiler.hooks.done.tap(this.constructor.name, () => {
+      const totalSize = Array.from(this.processed).reduce((pre, cur) => {
+        return pre + statSync(cur).size;
+      }, 0);
+
+      logger.info(
+        `${this.constructor.name} has reduced ${
+          this.processed.size
+        } duplicate modules, Total file size: ${formatBytes(
+          totalSize
+        )}(Estimated value, may not be accurate):\n${Array.from(
+          this.processed
+        ).join("\n")}`
+      );
+    });
+  }
+}
+
+module.exports = WebpackDontRepackPlugin;
